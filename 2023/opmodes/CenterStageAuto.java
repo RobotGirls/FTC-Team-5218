35b767ae709624e88291540c871dfdfbc7cf1213
Madelyn <madelyntran@rocknrollrobots25.com>
2023-10-02 20:08:27 -0700

    added auto with vision portal incorporated still in progress

---
+++ 2023/opmodes/CenterStageAuto.java
@@ -0,0 +1,46 @@
+//package opmodes;
+//import com.qualcomm.robotcore.util.RobotLog;
+//
+import team25core.ObjectDetectionNewTask;
+//import team25core.Robot;
+//import team25core.RobotEvent;
+//
+//
+//public class CenterStageAuto extends Robot {
+//    private ObjectDetectionNewTask objDetectionTask;
+//    private final static String TAG = "Prop";
+//    ObjectDetectionNewTask objDetectionTask;
+//
+//    public void detectProp()
+//    {
+//        RobotLog.ii(TAG, "Setup detectProp");
+//        objDetectionTask = new ObjectDetectionNewTask(this){
+//            @Override
+//            public void handleEvent(RobotEvent e) {
+//                ObjectDetectionEvent event = (ObjectDetectionEvent) e;
+//                switch (event.kind) {
+//                    case OBJECTS_DETECTED:
+//                        RobotLog.ii(TAG, "Object detected");
+//                        break;
+//                }
+//            }
+//        };
+//        //FIXME figure out where we want to do the addTask
+//        //addTask(new SkystoneDetectionTask(this, purpleColorSensor, purpleDistanceSensor) {
+//
+//}
+//
+//
+//
+//    @Override
+//   public void init(){
+//
+//   }
+//   @Override
+//   public void start(){
+//       objDetectionTask = new ObjectDetectionNewTask();
+//
+//   }
+//
+//
+//}
