e64136f0cb346c33eadf198b414c0a5c6964c0a6
Madelyn <madelyntran@rocknrollrobots25.com>
2023-10-07 15:49:03 -0700

    In progress of Distance Sensors for Prop Detections(added the telemetry)

---
+++ 2024/opmodes/DistanceSensorObjectDetection.java
@@ -0,0 +1,100 @@
+package opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import team25core.DeadReckonPath;
+import team25core.DeadReckonTask;
+import team25core.FourWheelDirectDrivetrain;
+import team25core.Robot;
+import team25core.RobotEvent;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import team25core.DistanceSensorCriteria;
+import team25core.vision.apriltags.AprilTagDetectionTask;
+
+@Autonomous(name = "DistanceSensorProp")
+public class DistanceSensorObjectDetection extends Robot {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    private FourWheelDirectDrivetrain drivetrain;
+
+    private DistanceSensor rightSensor;
+    private DistanceSensor leftSensor;
+    private Telemetry.Item tagIdTlm;
+    private Telemetry.Item rightSensorTlm;
+    private Telemetry.Item leftSensorTlm;
+    private Telemetry.Item whereAmI;
+    private Telemetry.Item eventTlm;
+
+    double rightDistance;
+    double leftDistance;
+
+
+    @Override
+    public void handleEvent(RobotEvent e)
+    {
+        whereAmI.setValue("in handleEvent");
+        /*
+         * Every time we complete a segment drop a note in the robot log.
+         */
+        if (e instanceof DeadReckonTask.DeadReckonEvent) {
+            RobotLog.i("Completed path segment %d", ((DeadReckonTask.DeadReckonEvent)e).segment_num);
+        }
+    }
+
+
+    public void init()
+    {
+        // hardware mapping
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        rightSensor = hardwareMap.get(DistanceSensor.class, "rightSensor");
+        leftSensor = hardwareMap.get(DistanceSensor.class, "leftSensor");
+
+        // instantiating FourWheelDirectDrivetrain
+        drivetrain = new FourWheelDirectDrivetrain(frontRight, backRight, frontLeft, backLeft);
+
+        //sets motors position to 0
+        drivetrain.resetEncoders();
+
+        //motor will try to tun at the targeted velocity
+        drivetrain.encodersOn();
+
+        // telemetry shown on the phone
+        whereAmI = telemetry.addData("location in code", "init");
+        tagIdTlm = telemetry.addData("tagId", "none");
+        rightSensorTlm = telemetry.addData("rightSensor", "none");
+        leftSensorTlm = telemetry.addData("leftSensor", "none");
+
+
+        initPaths();
+    }
+
+    public void start()
+    {
+        whereAmI.setValue("in Start");
+        while(true){
+            leftDistance = leftSensor.getDistance(DistanceUnit.CM);
+            rightDistance = rightSensor.getDistance(DistanceUnit.CM);
+            rightSensorTlm.setValue(rightDistance);
+            leftSensorTlm.setValue(leftDistance);
+        }
+
+    }
+
+    public void initPaths() {
+
+    }
+}
+
+
