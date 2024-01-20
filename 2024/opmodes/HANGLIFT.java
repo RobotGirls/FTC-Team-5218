4b966ade51b5b6b285b494892a6aeee1300a21d3
Madelyn <madelyntran@rocknrollrobots25.com>
2024-01-20 10:18:28 -0800

    made hanglift on joystick

--- 2024/opmodes/HANGLIFT.java
+++ 2024/opmodes/HANGLIFT.java
@@ -45,13 +45,13 @@ import team25core.MechanumGearedDrivetrain;
 import team25core.OneWheelDriveTask;
 import team25core.RobotEvent;
 import team25core.StandardFourMotorRobot;
-import team25core.TwoStickMechanumControlScheme;
 import team25core.TeleopDriveTask;
+import team25core.TwoStickMechanumControlScheme;
 
 
-@TeleOp(name = "TwoStickTeleop")
+@TeleOp(name = "HANGLIFT")
 //@Disabled
-public class TwoStickTeleop extends StandardFourMotorRobot {
+public class HANGLIFT extends StandardFourMotorRobot {
 
     private TeleopDriveTask drivetask;
 
@@ -59,40 +59,22 @@ public class TwoStickTeleop extends StandardFourMotorRobot {
         CLOCKWISE,
         COUNTERCLOCKWISE,
     }
-    //added field centric
-    private Telemetry.Item buttonTlm;
 
-    private static final double DRONE_SET_LEFT = 0.95;
-    private static final double DRONE_SET_RIGHT = 0;
-    private static final double DRONE_RELEASE = 0.5;
-    private static final int HANGING_FULLY_EXTENDED_RIGHT= 9856;
-    private static final int HANGING_FULLY_RETRACTED_RIGHT = 5;
-    private static final int HANGING_FULLY_EXTENDED_LEFT = 7156;
-    private static final int HANGING_FULLY_RETRACTED_LEFT = 5;
-    private static final double CLAW_OPEN = 0.5;
-    private static final double CLAW_CLOSE = 0.2;
 
     private BNO055IMU imu;
 
-    private Servo clawServo;
+
 
     private DcMotor hangingMotorLeft;
     private DcMotor hangingMotorRight;
 
-    private DcMotor liftMotor;
 
-    //    private DcMotor intakeMotor;
-    private DcMotor transportMotor;
     private boolean currentlySlow = false;
-    private OneWheelDriveTask liftMotorTask;
-    private OneWheelDriveTask transporIntakeMotorTask;
 
 
-    MecanumFieldCentricDriveScheme scheme;
+    private OneWheelDriveTask hangingMotorLeftTask;
+    private OneWheelDriveTask hangingMotorRightTask;
 
-    private Servo droneServoLeft;
-    private Servo droneServoRight;
-    private MechanumGearedDrivetrain drivetrain;
 
     @Override
     public void handleEvent(RobotEvent e) {
@@ -106,71 +88,22 @@ public class TwoStickTeleop extends StandardFourMotorRobot {
         hangingMotorLeft = hardwareMap.get(DcMotor.class,"hangingMotorLeft");
         hangingMotorRight = hardwareMap.get(DcMotor.class,"hangingMotorRight");
 
-        clawServo = hardwareMap.servo.get("clawServo");
 
-//        intakeMotor =  hardwareMap.get(DcMotor.class,"intakeMotor");
-        transportMotor  =  hardwareMap.get(DcMotor.class,"transportMotor");
 
-        clawServo = hardwareMap.servo.get("clawServo");
-        liftMotor = hardwareMap.get(DcMotor.class,"liftMotor");
+        //hangingMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+       // hangingMotorLeft.setPower(0.75);
 
-        droneServoLeft = hardwareMap.servo.get("droneServoLeft");
-        droneServoRight = hardwareMap.servo.get("droneServoRight");
+       // hangingMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+       // hangingMotorRight.setPower(0.75);
 
-        // using encoders to record ticks
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        droneServoLeft.setPosition(DRONE_SET_LEFT);
-        droneServoRight.setPosition(DRONE_SET_RIGHT);
+        hangingMotorLeftTask = new OneWheelDriveTask(this, hangingMotorLeft, true);
+        hangingMotorLeftTask.slowDown(true);
+        hangingMotorRightTask = new OneWheelDriveTask(this, hangingMotorRight, false);
+        hangingMotorRightTask.slowDown(true);
 
-        // the motor must be at its set position zero, at the beginning of the opmode
-        hangingMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        hangingMotorLeft.setTargetPosition(0);
-        // encoder allows you to know how much the motor has spun (distance)
-        hangingMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        // the brake allows the motor to hold its position when power is not currently being applied
-        hangingMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        hangingMotorLeft.setPower(0.75);
-        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        hangingMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        hangingMotorRight.setTargetPosition(0);
-        // encoder allows you to know how much the motor has spun (distance)
-        hangingMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        // the brake allows the motor to hold its position when power is not currently being applied
-        hangingMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        hangingMotorRight.setPower(0.75);
 
-//        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        transportMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        transportMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        clawServo.setPosition(CLAW_CLOSE);
-
-        //telemetry
-        buttonTlm = telemetry.addData("buttonState", "unknown");
-
-        TwoStickMechanumControlScheme scheme = new TwoStickMechanumControlScheme(gamepad1);
-        drivetrain = new MechanumGearedDrivetrain(motorMap);
-        drivetrain.setNoncanonicalMotorDirection();
-        // Note we are swapping the rights and lefts in the arguments below
-        // since the gamesticks were switched for some reason and we need to do
-        // more investigation
-        liftMotorTask = new OneWheelDriveTask(this, liftMotor, true);
-        liftMotorTask.slowDown(false);
-
-        transporIntakeMotorTask = new OneWheelDriveTask(this, transportMotor, false);
-        transporIntakeMotorTask.slowDown(false);
-
-        drivetask = new TeleopDriveTask(this, scheme, frontLeft, frontRight, backLeft, backRight);
     }
 
     public void initIMU()
@@ -188,10 +121,10 @@ public class TwoStickTeleop extends StandardFourMotorRobot {
     @Override
     public void start() {
 
-        //Gamepad 1
-        this.addTask(drivetask);
-        this.addTask(liftMotorTask);
-        this.addTask(transporIntakeMotorTask);
+
+        this.addTask(hangingMotorLeftTask);
+        this.addTask(hangingMotorRightTask);
+
 
 
         this.addTask(new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_1) {
@@ -219,7 +152,7 @@ public class TwoStickTeleop extends StandardFourMotorRobot {
                         // If slow, then normal speed. If fast, then slow speed of motors.
                         //pertains to slowmode
                         if (currentlySlow) {
-                            drivetask.slowDown(0.5);
+                            drivetask.slowDown(0.4);
                             currentlySlow = false;
                         } else {
                             drivetask.slowDown(0.1);
@@ -240,49 +173,9 @@ public class TwoStickTeleop extends StandardFourMotorRobot {
                 GamepadEvent gamepadEvent = (GamepadEvent) e;
 
                 switch (gamepadEvent.kind) {
-                    case RIGHT_BUMPER_DOWN:
-                        droneServoLeft.setPosition(DRONE_RELEASE);
-                        droneServoRight.setPosition(DRONE_RELEASE);
-                        break;
-                    case LEFT_TRIGGER_DOWN:
-                        // set claw's position to 0
-                        clawServo.setPosition(CLAW_CLOSE);
-                        break;
-                    case RIGHT_TRIGGER_DOWN:
-                        // set claw's position to 1
-                        clawServo.setPosition(CLAW_OPEN);
-                        break;
-                    case BUTTON_Y_DOWN:
-                        // set arm to extend to its highest capacity to lift robot
-                        hangingMotorRight.setTargetPosition(HANGING_FULLY_EXTENDED_RIGHT);
-                        hangingMotorLeft.setTargetPosition(HANGING_FULLY_EXTENDED_LEFT);
 
-                        break;
-                    case BUTTON_A_DOWN:
-                        // set arm to retract to its lowest capacity to lift robot
-                        hangingMotorRight.setTargetPosition(HANGING_FULLY_RETRACTED_RIGHT);
-                        hangingMotorLeft.setTargetPosition(HANGING_FULLY_RETRACTED_LEFT);
-                        break;
-
-//                    case BUTTON_X_DOWN:
-//                        transportMotor.setPower(1);
-//                        // intake pixels into robot
-//                        break;
-//                    case BUTTON_X_UP:
-////
-//                        transportMotor.setPower(0);
-////                    // outtakes pixels out of robot
-//                        break;
-//                    case BUTTON_B_DOWN:
-//                        transportMotor.setPower(-1);
-//                        // outtakes pixels out of robot
-//                        break;
-//                    case BUTTON_B_UP:
-//                        transportMotor.setPower(0);
-//                        // outtakes pixels out of robot
-//                        break;
                     default:
-                        buttonTlm.setValue("Not Moving");
+                       // buttonTlm.setValue("Not Moving");
                         break;
                 }
             }
